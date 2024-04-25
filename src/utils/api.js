9c4093f4c60e8a542ded2190a650476f8ab0dc54
Chetanya-Raj-Verma <139340894+Chetanya-Raj-Verma@users.noreply.github.com>
2024-04-25 19:19:05 +0530

    error fixed

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = "https://api.themoviedb.org/3";
-const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
+const TMDB_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyOTY0OWYzZGRhZGM1ZDY0ZmMyODFkMmUyZWUyMDc5ZCIsInN1YiI6IjY2MWY5OTBhYzA5ZTk5MDE2M2FmZGI5OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hP_Za2txqJAn0PHezP68WFnib0dD1MLOrXhD_Grf4aE";
 
 const headers = {
     Authorization: "bearer " + TMDB_TOKEN,
